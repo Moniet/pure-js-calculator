042eb3b1206850d5f9aa5b8dd4f2080b1df57686
pantingpanda <moniet.sawhney@gmail.com>
2018-07-29 16:21:15 +0100

    gulpfile update to 4.0.0alpha with syntax changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,25 @@
-const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-const cleanCSS = require('clean-css');
-const pump = require('pump');
-const rename = require('gulp-rename');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
-var lokesh = {
-
-  scripts: "assets/scripts/main.js",
-  css: "assets/css/style.css"
-
-};
-
-
-gulp.task('js-min', function (cb) {
-
-   return gulp.src(lokesh.scripts.toString())
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(rename(function (path) {
-        path.basename += 'min';
-        path.extname += '.js';
-      }))
-      .pipe(gulp.dest('scripts'))
-      .on('error', function(err) {
-        console.error('Error in script task', err.toString());
-      });
-
+gulp.task('js-min', function () {
+  return gulp.src('assets/scripts/main.js')
+    .pipe(uglify())
+    .pipe(rename(function (path) {
+      path.basename += '.min';
+    }))
+    .pipe(gulp.dest('assets/scripts'))
+    .on('error', function(err) {
+      console.error('Error in script task', err.toString());
+  });
 });
 
 gulp.task('css-min', function() {
-
-  return gulp.src(lokesh.css.toString())
-    .path(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename(function (path) {
-      path.basename += 'min';
-      path.extname += '.css';
-    }))
-   .pipe(gulp.dest(lokesh.css))
-   .on('error', function(err) {
-     console.error('error in css min task', err.toString());
-   });
-
+  return gulp.src('assets/css/style.css')
+  .pipe(cleanCSS({compatibility: '*'}))
+  .pipe(rename(function (mycss) {
+     mycss.basename += '.min';
+  }))
+  .pipe(gulp.dest('assets/css'));
 });
-
-gulp.task('lookout', function() {
-
-  gulp.watch(lokesh.css.toString(), ['css-min']);
-  gulp.watch(lokesh.scripts.toString(), ['js-min']);
-
-});
-
-gulp.task('comp', ['js-min', 'css-min', 'lookout']);
