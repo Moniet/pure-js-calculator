6c3ecbd2cd4af6b7df44753171c0e6e5359f96b7
moniet <moniet.sawhney@gmail.com>
2019-04-12 11:23:49 +0100

    fixed banner font size

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,32 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('minify-css', (cb) => {
+function minCSS(cb) {
   return gulp.src('./assets/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./assets/css/min-css'));
   cb();
-});
+}
 
 var uglify = require('gulp-uglify');
 var pipeline = require('readable-stream').pipeline;
 
-gulp.task('minify-js', () => {
+function minJS(cb) {
   return pipeline(
         gulp.src('.assets/scripts/*.js'),
         uglify(),
         gulp.dest('.assets/scripts/min-js')
   );
-});
+  cb();
+}
 
-gulp.task('default', gulp.series('minify-css', 'minify-js'));
+function watcher(cb) {
+  gulp.watch('./assets/css/style.css', minCSS);
+  gulp.watch('./assets/js/main.js', minJS);
+}
+
+gulp.task('min-css', minCSS);
+gulp.task('min-js', minJS);
+gulp.task('watcher', watcher);
+
+gulp.task('default', gulp.series('min-css', 'min-js', 'watcher'));
